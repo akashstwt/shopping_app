4b0090b5ff963bc6bbaab2faaba00e3d9ff4be90
akashstwt <akashpatel115347@gmail.com>
2024-05-16 16:06:04 +0530

    search bar added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,22 +3,58 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
+    );
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Text(
-              ' Shoes\n Collection',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 35,
-              ),
-            )
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    'Shoes\nCollection',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      border: border,
+                      enabledBorder: border,
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                  itemCount: filters.length,
+                  itemBuilder: (context, index) {
+                    final filter = filters[index];
+                    return Chip(
+                      label: Text(
+                        filter,
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
           ],
         ),
-      ) ,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shopping App',
       theme: ThemeData(
         fontFamily: 'Lato',
         colorScheme:ColorScheme.fromSeed(
-          seedColor: const Color.fromRGBO(254, 206, 1, 1)), 
+          seedColor: const Color.fromRGBO(254, 206, 1, 1)),
+          inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize:16,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1)
+            
+          ) 
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
