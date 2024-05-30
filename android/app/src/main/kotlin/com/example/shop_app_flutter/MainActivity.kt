7375a9ef437b6455d482d9630c86a351c79f3188
akashstwt <akashpatel115347@gmail.com>
2024-05-31 04:54:53 +0530

    final update

--- android/app/src/main/kotlin/com/example/shop_app_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shop_app_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.shopping_app
+package com.example.shop_app_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping_app/cart_provider.dart';
-import 'package:shopping_app/global_variables.dart';
+import 'package:shopping_app/providers/cart_provider.dart';
 
 class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
@@ -29,12 +28,12 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           'size': selectedSize,
        },
       );
-    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Product added succesfully'),
         ),
       );
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Please select a size'),
@@ -61,7 +60,10 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
           const Spacer(),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Image.asset(widget.product['imageUrl'] as String),
+            child: Image.asset(
+              widget.product['imageUrl'] as String,
+              height:250,
+            ),
           ),
           const Spacer(flex: 2),
           Container(
@@ -112,7 +114,7 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
                     onPressed: onTap,
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Theme.of(context).colorScheme.primary,
-                      minimumSize: const Size(double.infinity, 50),
+                      fixedSize: const Size(350, 50),
                     ),
                     child: const Row(
                       mainAxisSize: MainAxisSize.min,
